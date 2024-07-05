45d0458805061f4234c1a0b7f703e75c4cae113a
Ubuntu <ubuntu@ip-10-240-230-129.us-west-2.compute.internal>
2024-07-05 10:38:58 +0000

    Initial commit of project files

---
+++ main/build/default/_generated_5251b6926a464026.go
@@ -0,0 +1,15 @@
+components {
+  id: "factory"
+  component: "/main/zombie.go"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
